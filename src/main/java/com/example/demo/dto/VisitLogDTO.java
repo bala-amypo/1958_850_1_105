ca368a01f6d75d77359cc5faba0400308f5e17bc
bala-amypo <bala@amypo.in>
2025-12-25 10:57:35 +0000

    User commit

--- src/main/java/com/example/demo/dto/VisitLogDTO.java
+++ src/main/java/com/example/demo/dto/VisitLogDTO.java
@@ -1,8 +1,9 @@
 package com.example.demo.dto;
-import jakarta.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Data
@@ -10,19 +11,11 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class VisitLogDTO {
     private Long id;
-
     private Long visitorId;
-
     private Long hostId;
-
     private LocalDateTime checkInTime;
-
     private LocalDateTime checkOutTime;
-
-    @NotBlank(message = "Purpose is required")
     private String purpose;
-
     private Boolean accessGranted;
-
     private Boolean alertSent;
 }
