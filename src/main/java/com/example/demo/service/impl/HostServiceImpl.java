e332ad8bb74bf9b6e8bec8bca9d43964e7d25c4b
bala-amypo <bala@amypo.in>
2025-12-26 08:04:11 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/HostServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/HostServiceImpl.java
@@ -5,28 +5,36 @@ import com.example.demo.repository.HostRepository;
 import com.example.demo.service.HostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class HostServiceImpl implements HostService {
-    
+
     private HostRepository hostRepository;
-    
+
     public HostServiceImpl() {}
-    
+
     @Autowired
     public void setHostRepository(HostRepository hostRepository) {
         this.hostRepository = hostRepository;
     }
-    
+
     @Override
     public Host createHost(Host host) {
         return hostRepository.save(host);
     }
-    
+
     @Override
     public Host getHost(Long id) {
         Optional<Host> host = hostRepository.findById(id);
         return host.orElseThrow(() -> new RuntimeException("Host not found with id: " + id));
+    }
+
+    // IMPLEMENTATION FOR getAllHosts()
+    @Override
+    public List<Host> getAllHosts() {
+        return hostRepository.findAll();
     }
 }
